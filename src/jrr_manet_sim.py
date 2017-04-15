fc9427ee91d126be30fb87aa2f7775a2fd48ffda
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2017-04-14 23:20:20 -0400

    refactor into ManetSimulator class

--- src/jrr_manet_sim.py
+++ src/jrr_manet_sim.py
@@ -35,65 +35,122 @@ NUM_SINKS           = 10
 UDP_PORT            = 9
 TOTAL_TIME          = 200.0 # sec
 
-def setup_wifi_phy():
-    chanhlp = ns.wifi.YansWifiChannelHelper()
-    chanhlp.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel")
-    chanhlp.AddPropagationLoss("ns3::FriisPropagationLossModel")
-
-    phy = ns.wifi.YansWifiPhyHelper.Default()
-    phy.SetChannel(chanhlp.Create())
-
-    txp = DoubleValue(WIFI_TX_POWER)
-    phy.Set("TxPowerStart", txp)
-    phy.Set("TxPowerEnd", txp)
-
-    return phy
-
-def setup_wifi_mac():
-    """Set up non-QoS MAC"""
-    mac = ns.wifi.NqosWifiMacHelper.Default()
-    mac.SetType("ns3::AdhocWifiMac")
-    return mac
-
-def setup_mobility():
-    mobility = ns.mobility.MobilityHelper()
-
-    # Set up the grid
-    # Objects are layed out starting from (-100, -100)
-    mobility.SetPositionAllocator(
-            "ns3::GridPositionAllocator",
-            "MinX", DoubleValue(-100.0),
-            "MinY", DoubleValue(-100.0),
-            "DeltaX", DoubleValue(NODE_X_INTERVAL),
-            "DeltaY", DoubleValue(NODE_Y_INTERVAL),
-            "GridWidth", UintegerValue(NODES_PER_ROW),
-            "LayoutType", StringValue("RowFirst"),
-            )
-
-    # Objects will be in a fixed position throughout the simulation
-    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel")
-
-    return mobility
-
-def setup_routing(args):
-    protocol = protocol_map[args.protocol]()
-    route_list = ns.internet.Ipv4ListRoutingHelper()
-    route_list.Add(protocol, 100)
-
-    inet = ns.internet.InternetStackHelper()
-    inet.SetRoutingHelper(route_list)
-    return inet
-
-def setup_packet_receive(sockaddr, node):
-    tid = ns.core.TypeId.LookupByName("ns3::UdpSocketFactory")
-    sink = ns.network.Socket.CreateSocket(node, tid)
-    sink.Bind(sockaddr)
-    sink.SetRecvCallback(packet_rx_callback)
-    return sink
+class ManetSimulator(object):
+    def __init__(self, num_nodes, protocol):
+        self._setup(num_nodes, protocol)
 
 
-def packet_rx_callback(*args, **kwargs):
-    logging.debug("Packet callback: args={} kwargs={}".format(args, kwargs))
+    def _setup(self, num_nodes, protocol):
+        # Create a container with the desired number of nodes
+        self.nodes = ns.network.NodeContainer();
+        self.nodes.Create(num_nodes)
+
+        # Set up Wifi devices
+        phyMode = StringValue("DsssRate11Mbps")
+
+        wifi = ns.wifi.WifiHelper()
+        wifi.SetStandard(ns.wifi.WIFI_PHY_STANDARD_80211b)
+        wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
+                                     "DataMode", phyMode,
+                                     "ControlMode", phyMode)
+
+        wifiPhy = self._setup_wifi_phy()
+        wifiMac = self._setup_wifi_mac()
+        adhocDevices = wifi.Install(wifiPhy, wifiMac, self.nodes)
+
+        # Set up mobility
+        mobility = self._setup_mobility()
+        mobility.Install(self.nodes)
+
+        # Set up routing
+        inet = self._setup_routing(protocol)
+        inet.Install(self.nodes)
+
+        # Assign IP addresses
+        addrs = ns.internet.Ipv4AddressHelper()
+        addrs.SetBase(
+            ns.network.Ipv4Address("10.1.1.0"),
+            ns.network.Ipv4Mask("255.255.255.0"))
+        ifaces = addrs.Assign(adhocDevices)
+
+        onoff = ns.applications.OnOffHelper("ns3::UdpSocketFactory", ns.network.Address())
+        onoff.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1.0]"))
+        onoff.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0.0]"))
+
+        # Set up the source/sink nodes
+        for i in xrange(NUM_SINKS):
+            node = self.nodes.Get(i)
+            sockaddr = ns.network.InetSocketAddress(ifaces.GetAddress(i), UDP_PORT)
+
+            self._setup_packet_receive(sockaddr, node)
+
+            onoff.SetAttribute("Remote", ns.network.AddressValue(sockaddr))
+            temp = onoff.Install(self.nodes.Get(i + NUM_SINKS))
+
+            var = ns.core.UniformRandomVariable()
+            temp.Start(Seconds(var.GetValue(100.0, 101.0)))
+            temp.Stop(Seconds(TOTAL_TIME))
+
+
+    def _setup_wifi_phy(self):
+        chanhlp = ns.wifi.YansWifiChannelHelper()
+        chanhlp.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel")
+        chanhlp.AddPropagationLoss("ns3::FriisPropagationLossModel")
+
+        phy = ns.wifi.YansWifiPhyHelper.Default()
+        phy.SetChannel(chanhlp.Create())
+
+        txp = DoubleValue(WIFI_TX_POWER)
+        phy.Set("TxPowerStart", txp)
+        phy.Set("TxPowerEnd", txp)
+
+        return phy
+
+    def _setup_wifi_mac(self):
+        """Set up non-QoS MAC"""
+        mac = ns.wifi.NqosWifiMacHelper.Default()
+        mac.SetType("ns3::AdhocWifiMac")
+        return mac
+
+    def _setup_mobility(self):
+        mobility = ns.mobility.MobilityHelper()
+
+        # Set up the grid
+        # Objects are layed out starting from (-100, -100)
+        mobility.SetPositionAllocator(
+                "ns3::GridPositionAllocator",
+                "MinX", DoubleValue(-100.0),
+                "MinY", DoubleValue(-100.0),
+                "DeltaX", DoubleValue(NODE_X_INTERVAL),
+                "DeltaY", DoubleValue(NODE_Y_INTERVAL),
+                "GridWidth", UintegerValue(NODES_PER_ROW),
+                "LayoutType", StringValue("RowFirst"),
+                )
+
+        # Objects will be in a fixed position throughout the simulation
+        mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel")
+
+        return mobility
+
+    def _setup_routing(self, protocol_name):
+        protocol = protocol_map[protocol_name]()
+        route_list = ns.internet.Ipv4ListRoutingHelper()
+        route_list.Add(protocol, 100)
+
+        inet = ns.internet.InternetStackHelper()
+        inet.SetRoutingHelper(route_list)
+        return inet
+
+    def _setup_packet_receive(self, sockaddr, node):
+        tid = ns.core.TypeId.LookupByName("ns3::UdpSocketFactory")
+        sink = ns.network.Socket.CreateSocket(node, tid)
+        sink.Bind(sockaddr)
+        sink.SetRecvCallback(self._packet_rx_callback)
+        return sink
+
+    def _packet_rx_callback(self, *args, **kwargs):
+        logging.debug("Packet callback: args={} kwargs={}".format(args, kwargs))
+
 
 protocol_map = {
     'AODV':     ns.aodv.AodvHelper,
@@ -119,56 +176,10 @@ def main():
     args = parse_args()
     logging.basicConfig(level=args.loglevel)
 
-
-    # Create a container with the desired number of nodes
-    adhocNodes = ns.network.NodeContainer();
-    adhocNodes.Create(args.num_nodes)
-
-    # Set up Wifi devices
-    phyMode = StringValue("DsssRate11Mbps")
-
-    wifi = ns.wifi.WifiHelper()
-    wifi.SetStandard(ns.wifi.WIFI_PHY_STANDARD_80211b)
-    wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
-                                 "DataMode", phyMode,
-                                 "ControlMode", phyMode)
-
-    wifiPhy = setup_wifi_phy()
-    wifiMac = setup_wifi_mac()
-    adhocDevices = wifi.Install(wifiPhy, wifiMac, adhocNodes)
-
-    # Set up mobility
-    mobility = setup_mobility()
-    mobility.Install(adhocNodes)
-
-    # Set up routing
-    inet = setup_routing(args)
-    inet.Install(adhocNodes)
-
-    # Assign IP addresses
-    addrs = ns.internet.Ipv4AddressHelper()
-    addrs.SetBase(
-        ns.network.Ipv4Address("10.1.1.0"),
-        ns.network.Ipv4Mask("255.255.255.0"))
-    ifaces = addrs.Assign(adhocDevices)
-
-    onoff = ns.applications.OnOffHelper("ns3::UdpSocketFactory", ns.network.Address())
-    onoff.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1.0]"))
-    onoff.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0.0]"))
-
-    # Set up the source/sink nodes
-    for i in xrange(NUM_SINKS):
-        node = adhocNodes.Get(i)
-        sockaddr = ns.network.InetSocketAddress(ifaces.GetAddress(i), UDP_PORT)
-
-        setup_packet_receive(sockaddr, node)
-
-        onoff.SetAttribute("Remote", ns.network.AddressValue(sockaddr))
-        temp = onoff.Install(adhocNodes.Get(i + NUM_SINKS))
-
-        var = ns.core.UniformRandomVariable()
-        temp.Start(Seconds(var.GetValue(100.0, 101.0)))
-        temp.Stop(Seconds(TOTAL_TIME))
+    sim = ManetSimulator(
+            num_nodes = args.num_nodes,
+            protocol = args.protocol,
+            )
 
 
     trace = ns.network.AsciiTraceHelper()
